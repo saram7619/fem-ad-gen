968b8a4777b9ee581110b0455e903de4757ca976
Sarah McClelland <saramcclelland@icloud.com>
2023-05-20 21:59:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Advice from "./components/Advice"
+
+function App() {
+  return (
+    <main className='App'>
+      <>
+        <Advice />
+      </>
+    </main>
+  )
+}
+
+export default App
