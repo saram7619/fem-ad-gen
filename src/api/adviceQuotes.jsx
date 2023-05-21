908a9948784fea4f95290f34820a2545493f0242
Sarah McClelland <saramcclelland@icloud.com>
2023-05-21 12:43:16 +0100

    Fixed button image and readme

--- src/api/adviceQuotes.jsx
+++ src/api/adviceQuotes.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 const BASE_URL = "https://api.adviceslip.com"
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
